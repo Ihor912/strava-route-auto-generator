5e79a7c04eab2e85cba6be8a176509e572769d8c
ihor-huma <ihor.khomiak@huma.com>
2024-05-19 08:52:17 +0200

    feat: use env variables

--- app/hooks/useActivities.ts
+++ app/hooks/useActivities.ts
@@ -14,14 +14,14 @@ export function useActivities() {
     async function fetchActivities() {
       try {
         const authResponse = await axios.post(
-          `${process.env.STRAVA_API_BASE_URL}/oauth/token?client_id=${process.env.STRAVA_CLIENT_ID}&client_secret=${process.env.STRAVA_CLIENT_SECRET}&refresh_token=${process.env.STRAVA_REFRESH_TOKEN}&grant_type=refresh_token`
+          `${process.env.NEXT_PUBLIC_STRAVA_API_BASE_URL}/oauth/token?client_id=${process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID}&client_secret=${process.env.NEXT_PUBLIC_STRAVA_CLIENT_SECRET}&refresh_token=${process.env.NEXT_PUBLIC_STRAVA_REFRESH_TOKEN}&grant_type=refresh_token`
         );
         if (authResponse.status !== 200) {
           throw new Error("Network response was not ok");
         }
         if (authResponse?.data && authResponse.data?.access_token) {
           const activitiesResponse = await axios.get(
-            `${process.env.STRAVA_API_BASE_URL}/api/v3/athlete/activities?access_token=${authResponse.data.access_token}&per_page=200`
+            `${process.env.NEXT_PUBLIC_STRAVA_API_BASE_URL}/api/v3/athlete/activities?access_token=${authResponse.data.access_token}&per_page=200`
           );
 
           const activities = activitiesResponse.data as ActivityResponse[];
