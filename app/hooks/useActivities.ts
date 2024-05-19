d379ec64ff3ea0d2edb0c86ffcd1f50969e329ce
ihor-huma <ihor.khomiak@huma.com>
2024-05-19 02:22:25 +0200

    feat: use env variables

--- app/hooks/useActivities.ts
+++ app/hooks/useActivities.ts
@@ -4,8 +4,6 @@ import { ActivitiesContext } from "../context/activities-context";
 import axios from "axios";
 import polyline from "@mapbox/polyline";
 
-const API_BASE_URL = "https://www.strava.com";
-
 export function useActivities() {
   // store activities in context and fetch new data only in case if it's empty.
   const { activities, setActivities } = useContext(ActivitiesContext);
@@ -16,14 +14,14 @@ export function useActivities() {
     async function fetchActivities() {
       try {
         const authResponse = await axios.post(
-          `${API_BASE_URL}/oauth/token?client_id=${process.env.STRAVA_CLIENT_ID}&client_secret=${process.env.STRAVA_CLIENT_SECRET}&refresh_token=${process.env.STRAVA_REFRESH_TOKEN}&grant_type=refresh_token`
+          `${process.env.STRAVA_API_BASE_URL}/oauth/token?client_id=${process.env.STRAVA_CLIENT_ID}&client_secret=${process.env.STRAVA_CLIENT_SECRET}&refresh_token=${process.env.STRAVA_REFRESH_TOKEN}&grant_type=refresh_token`
         );
         if (authResponse.status !== 200) {
           throw new Error("Network response was not ok");
         }
         if (authResponse?.data && authResponse.data?.access_token) {
           const activitiesResponse = await axios.get(
-            `${API_BASE_URL}/api/v3/athlete/activities?access_token=${authResponse.data.access_token}&per_page=200`
+            `${process.env.STRAVA_API_BASE_URL}/api/v3/athlete/activities?access_token=${authResponse.data.access_token}&per_page=200`
           );
 
           const activities = activitiesResponse.data as ActivityResponse[];
